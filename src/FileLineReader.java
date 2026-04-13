23ff7bbaeace213ed7cf04c7eda3bf98f015336b
kaali18 <kasinathkv6@gmail.com>
2026-04-13 23:19:26 +0530

    experiment-61-file-read-with-line-numbers

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -7,10 +7,16 @@ public class FileLineReader {
         String fileName = "input.txt";
         
         // TODO: Use a try-with-resources block to open a BufferedReader for the file
-        // TODO: Read the file line by line using a loop
-        // TODO: Keep track of the line number (starting from 1)
-        // TODO: Print each line in the format "lineNumber: lineContent"
-        // TODO: Catch and handle any IOExceptions that might occur
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            int lineNumber = 1;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(lineNumber + ": " + line);
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
         
         
     }
